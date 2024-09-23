620926365b93f3cbc30488fd2bbc1cd6723a6a26
Leonardo Nakamura <122413332+Leonardo5525@users.noreply.github.com>
2024-09-23 10:21:39 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const { log } = require('console')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log('New Connection')
+    res.end('Hello Awesome')
+})
+
+const PORT = process.env.PORT || 8080
+server.listen (PORT, () => console.log('Listening'))
